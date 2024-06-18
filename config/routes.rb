3fdf126d418160ebca2c14bb097679dab26fda20
anakettl <anakettl@gmail.com>
2024-06-18 18:32:40 -0300

    feat(products): create new route to import products spreadsheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "products#index"
+
+  post 'products/import', to: 'products#import'
+  resources :products
 end
